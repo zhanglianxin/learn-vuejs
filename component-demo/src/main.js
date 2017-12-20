300fb7213b3682d80d2e8ffb2aefe756192d3952
coolman <zhanglianxin1993@qq.com>
2017-12-20 09:30:39 +0800

    Register a global component

---
+++ component-demo/src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue';
+import App from './App.vue';
+import Home from './Home.vue';
+
+// 注册全局组件
+Vue.component('app-server-status', Home);
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
