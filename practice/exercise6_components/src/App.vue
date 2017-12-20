7d733688eb17374cf6ffd418c3dec12dff5ae351
coolman <zhanglianxin1993@qq.com>
2017-12-20 10:39:02 +0800

    exercise6_components

---
+++ practice/exercise6_components/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+        <app-header></app-header>
+        <hr>
+        <div class="row">
+            <!-- servers -->
+            <app-servers></app-servers>
+            <!-- server details -->
+            <app-server-details></app-server-details>
+        </div>
+        <hr>
+        <app-footer></app-footer>
+    </div>
+</template>
+
+<script>
+    import Header from './Header.vue';
+    import Footer from './Footer.vue';
+    import ServerDetails from './ServerDetails.vue';
+    import Servers from './Servers.vue';
+
+    export default {
+        components: {
+            'app-header': Header,
+            'app-footer': Footer,
+            'app-server-details': ServerDetails,
+            'app-servers': Servers,
+        },
+    };
+</script>
+
+<style>
+</style>
