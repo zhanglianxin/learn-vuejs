0ef3ca0ba0ff5666c656ef814a4822bed0be93cc
coolman <zhanglianxin1993@qq.com>
2017-12-21 13:38:48 +0800

    Centralizing code in an event bus

--- communicating-between-components/src/components/UserEdit.vue
+++ communicating-between-components/src/components/UserEdit.vue
@@ -20,7 +20,8 @@
         methods: {
             editAge() {
                 this.userAge = 30;
-                eventBus.$emit('ageWasEdited', this.userAge);
+                // eventBus.$emit('ageWasEdited', this.userAge);
+                eventBus.changeAge(this.userAge);
             },
         },
     };
