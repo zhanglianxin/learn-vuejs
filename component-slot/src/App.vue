03f5ae56b8e487f58d74bfbaa4a70021255a3263
coolman <zhanglianxin1993@qq.com>
2017-12-21 16:38:12 +0800

    Passing content with slots

---
+++ component-slot/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <app-quote>
+                    <h2>The Quote</h2>
+                    <p>A Wonderful Quote!</p>
+                </app-quote>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Quote from './components/Quote.vue';
+
+    export default {
+        components: {
+            'app-quote': Quote,
+        },
+    };
+</script>
+
+<style>
+</style>
