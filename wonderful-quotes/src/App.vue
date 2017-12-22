a1312aca6a44c96fd1a163433dd867c15eceb975
coolman <zhanglianxin1993@qq.com>
2017-12-22 13:42:55 +0800

    Complete wonderful quotes

---
+++ wonderful-quotes/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="container">
+        <app-header v-bind:quoteCount="quotes.length" v-bind:maxQuotes="maxQuotes"></app-header>
+        <app-newquote v-on:quoteAdded="newQuote"></app-newquote>
+        <app-quotegrid v-bind:quotes="quotes" v-on:quoteDeleted="deleteQuote"></app-quotegrid>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info">
+                    Info: Click on a Quote to delete it!
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import QuoteGrid from './components/QuoteGrid.vue';
+    import NewQuote from './components/NewQuote.vue';
+    import Header from './components/Header.vue';
+
+    export default {
+        components: {
+            'app-quotegrid': QuoteGrid,
+            'app-newquote': NewQuote,
+            'app-header': Header,
+        },
+
+        data() {
+            return {
+                quotes: [
+                    'Life is short, away from noisy!',
+                ],
+                maxQuotes: 10,
+            };
+        },
+
+        methods: {
+            newQuote(quote) {
+                if (this.quotes.length >= this.maxQuotes) {
+                    return alert('Please delete Quote first!');
+                }
+                this.quotes.push(quote);
+            },
+
+            deleteQuote(index) {
+                this.quotes.splice(index, 1);
+            },
+        },
+    };
+</script>
+
+<style>
+</style>
---
+++ wonderful-quotes/src/components/QuoteGrid.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="row">
+        <app-quote v-for="(quote, index) in quotes" v-bind:key="index" v-on:click.native="deleteQuote(index)">
+            {{ quote }}
+        </app-quote>
+    </div>
+</template>
+
+<script>
+    import Quote from './Quote.vue';
+
+    export default {
+        components: {
+            'app-quote': Quote,
+        },
+
+        props: [
+            'quotes',
+        ],
+
+        methods: {
+            deleteQuote(index) {
+                this.$emit('quoteDeleted', index);
+            },
+        },
+    };
+</script>
+
+<style>
+</style>
