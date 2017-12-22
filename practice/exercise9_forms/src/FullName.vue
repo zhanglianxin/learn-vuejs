9560a255d791773cedf529a598a51255ba3ee116
coolman <zhanglianxin1993@qq.com>
2017-12-22 17:49:04 +0800

    Practice - forms

---
+++ practice/exercise9_forms/src/FullName.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <div class="form-group">
+            <label>First Name</label>
+            <input type="text" class="form-control" v-bind:value="firstName"
+                v-on:change="nameChanged(true, $event)">
+        </div>
+        <div class="form-group">
+            <label>Last Name</label>
+            <input type="text" class="form-control" v-bind:value="lastName"
+                v-on:change="nameChanged(false, $event)">
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: [
+            'value',
+        ],
+
+        methods: {
+            nameChanged(isFirst, event) {
+                let name = '';
+
+                if (isFirst) {
+                    name = event.target.value + ' ' + this.lastName;
+                } else {
+                    name = this.firstName + ' ' + event.target.value;
+                }
+
+                // this.value = name;
+                // this.$emit('input', this.value);
+                this.$emit('input', name);
+            },
+        },
+
+        computed: {
+            firstName() {
+                return this.value.split(' ')[0] || '';
+            },
+
+            lastName() {
+                // Avoid undefined
+                return this.value.split(' ')[1] || '';
+            },
+        },
+    };
+</script>
+
+<style>
+</style>
