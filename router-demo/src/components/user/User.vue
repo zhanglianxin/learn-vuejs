6d71238900e45d32be6900a3cd0e2298a4c80dc9
coolman <zhanglianxin1993@qq.com>
2017-12-26 11:24:58 +0800

    Router demo

---
+++ router-demo/src/components/user/User.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1>The User Page</h1>
+        <hr>
+        <p>Loaded ID: {{ id }}</p>
+        <button v-on:click="navigateToHome" class="btn btn-primary">Go to Home</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                id: this.$route.params.id,
+            };
+        },
+
+        watch: {
+            '$route'(to, from) {
+                this.id = to.params.id;
+            },
+        },
+
+        methods: {
+            navigateToHome() {
+                this.$router.push('/');
+            },
+        },
+    };
+</script>
---
+++ router-demo/src/routes.js
@@ -0,0 +1,12 @@
+import User from './components/user/User.vue';
+import Home from './components/Home.vue';
+
+export const routes = [
+    {
+        path: '',
+        component: Home,
+    }, {
+        path: '/user/:id',
+        component: User,
+    }
+];
