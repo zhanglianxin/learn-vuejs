eb0eaac5830926f8f53597ec00d3935a2edefade
coolman <zhanglianxin1993@qq.com>
2017-12-26 15:34:04 +0800

    Animating route transitions

--- router-demo/src/App.vue
+++ router-demo/src/App.vue
@@ -6,7 +6,11 @@
                 <hr>
                 <!-- Using named views -->
                 <router-view name="header-top"></router-view>
-                <router-view></router-view>
+                <!-- Wrap the router view for which you want to any made with
+                    transition and then set up the transition -->
+                <transition name="slide" mode="out-in">
+                    <router-view></router-view>
+                </transition>
                 <router-view name="header-bottom"></router-view>
             </div>
         </div>
@@ -19,4 +23,36 @@
 </script>
 
 <style>
+    .slide-leave-active {
+        transition: opacity 1s ease;
+        opacity: 0;
+        animation: slide-out 1s ease-out forwards;
+    }
+
+    .slide-leave {
+        opacity: 1;
+        transform: translateX(0);
+    }
+
+    .slide-enter-active {
+        animation: slide-in 1s ease-out forwards;
+    }
+
+    @keyframes slide-out {
+        0% {
+            transform: translateY(0);
+        }
+        100% {
+            transform: translateY(-30px);
+        }
+    }
+
+    @keyframes slide-in {
+        0% {
+            transform: translateY(-30px);
+        }
+        100% {
+            transform: translateY(0);
+        }
+    }
 </style>
