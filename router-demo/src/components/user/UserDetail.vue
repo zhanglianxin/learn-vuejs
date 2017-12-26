23febf167e773cbb5cc98fe6012a7d9fe1af1cc9
coolman <zhanglianxin1993@qq.com>
2017-12-26 16:30:29 +0800

    Passing the hash fragment and controlling the scroll behavior

--- router-demo/src/components/user/UserDetail.vue
+++ router-demo/src/components/user/UserDetail.vue
@@ -4,9 +4,27 @@
         <p>User loaded has ID: {{ $route.params.id }}</p>
         <!-- Using query parameters -->
         <router-link tag="button" class="btn btn-primary"
-            v-bind:to="{
-                name: 'userEdit',
-                params: { id: $route.params.id },
-                query: { locale: 'en', q: 100 }, }">Edit User</router-link>
+            v-bind:to="link">Edit User</router-link>
     </div>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                link: {
+                    name: 'userEdit',
+                    params: {
+                        id: this.$route.params.id,
+                    },
+                    query: {
+                        locale: 'en',
+                        q: 100,
+                    },
+                    // Passing the hash fragment
+                    hash: '#data',
+                },
+            };
+        },
+    };
+</script>
--- router-demo/src/main.js
+++ router-demo/src/main.js
@@ -8,8 +8,19 @@ Vue.use(VueRouter);
 const router = new VueRouter({
     routes,
     mode: 'history',
+    // The third argument, savedPosition, is only available if this is a
+    // `popstate` navigation (triggered by the browser's back/forward buttons).
+    scrollBehavior(to, from, savedPosition) {
+        if (savedPosition) {
+            return savedPosition;
+        }
+        if (to.hash) {
+            return {
+                selector: to.hash,
+            };
+        }
+    },
 });
-
 
 new Vue({
   el: '#app',
