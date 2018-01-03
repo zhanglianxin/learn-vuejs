2ca86f9b5c46c748726b1b4352b6dc2b30998486
zhanglianxin <zhanglianxin1993@qq.com>
2018-01-03 22:35:43 +0800

    Mapping mutations to methods, using actions and mapping them to methods

--- vuex-demo/src/components/AnotherCounter.vue
+++ vuex-demo/src/components/AnotherCounter.vue
@@ -1,12 +1,13 @@
 <template>
     <div>
-        <button class="btn btn-primary" @click="increment">Increment</button>
-        <button class="btn btn-primary" @click="decrement">Decrement</button>
+        <button class="btn btn-primary" @click="asyncIncrement({ by: 50, duration: 500 })">Increment</button>
+        <button class="btn btn-primary" @click="asyncDecrement({ by: 50, duration: 500 })">Decrement</button>
     </div>
 </template>
 
 <script>
-    import { mapMutations } from 'vuex';
+    // import { mapMutations } from 'vuex';
+    import { mapActions } from 'vuex';
 
     export default {
         methods: {
@@ -18,9 +19,13 @@
                 // this.$store.state.counter--;
                 this.$store.commit('decrement')
             },*/
-            ...mapMutations([
+            /*...mapMutations([
                 'increment',
                 'decrement',
+            ]),*/
+            ...mapActions([
+                'asyncIncrement',
+                'asyncDecrement',
             ]),
         },
     };
--- vuex-demo/src/store/store.js
+++ vuex-demo/src/store/store.js
@@ -19,12 +19,37 @@ export const store = new Vuex.Store({
     },
 
     mutations: {
-        increment(state) {
-            state.counter++;
+        increment(state, payload) {
+            state.counter += payload;
         },
 
-        decrement(state) {
-            state.counter--;
+        decrement(state, payload) {
+            state.counter -= payload;
+        },
+    },
+
+    actions: {
+        /*increment(context) {
+            context.commit('increment');
+        },*/
+        increment({ commit }, payload) {
+            commit('increment', payload);
+        },
+
+        decrement({ commit }, payload) {
+            commit('decrement', payload);
+        },
+
+        asyncIncrement({ commit }, payload) {
+            setTimeout(() => {
+                commit('increment', payload.by);
+            }, payload.duration);
+        },
+
+        asyncDecrement({ commit }, payload) {
+            setTimeout(() => {
+                commit('decrement', payload.by);
+            }, payload.duration);
         },
     },
 });
