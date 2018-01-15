3004c38d9d6a5ab8ebcfb7bd67f3a6f94886b3a3
zhanglianxin <zhanglianxin1993@qq.com>
2018-01-15 22:22:37 +0800

    Adding a header and navigation, creating stocks components

--- the-stock-trader/src/App.vue
+++ the-stock-trader/src/App.vue
@@ -1,14 +1,26 @@
 <template>
     <div class="container">
-        <router-view></router-view>
+        <app-header></app-header>
+        <div class="row">
+            <div class="col-xs-12">
+                <router-view></router-view>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-    export default {
+    import Header from './components/Header.vue';
 
-    }
+    export default {
+        components: {
+            'app-header': Header,
+        },
+    };
 </script>
 
 <style>
+    body {
+        padding: 30px;
+    }
 </style>
