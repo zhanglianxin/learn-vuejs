6c8dde90154f59ed609c1196d7701a0ea81b0fb4
zhanglianxin <zhanglianxin1993@qq.com>
2018-01-24 22:05:43 +0800

    Animating the route transitions and adding a dropdown

--- the-stock-trader/src/components/Header.vue
+++ the-stock-trader/src/components/Header.vue
@@ -13,12 +13,13 @@
                 <strong class="navbar-text navbar-right">Funds: {{ funds | currency }}</strong>
                 <ul class="nav navbar-nav navbar-right">
                     <li><a href="#" v-on:click="endDay">End Day</a></li>
-                    <li class="dropdown">
+                    <li class="dropdown" v-bind:class="{ open: isDropdownOpen }"
+                        v-on:click="isDropdownOpen = !isDropdownOpen">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown"
                         role="button" aria-haspopup="true" aria-expanded="false"
                         >Save &amp; Load <span class="caret"></span></a>
                         <ul class="dropdown-menu">
-                            <li><a href="#">save Data</a></li>
+                            <li><a href="#">Save Data</a></li>
                             <li><a href="#">Load Data</a></li>
                         </ul>
                     </li>
@@ -32,6 +33,12 @@
     import { mapActions } from 'vuex';
 
     export default {
+        data() {
+            return {
+                isDropdownOpen: false,
+            };
+        },
+
         computed: {
             funds() {
                 return this.$store.getters.funds;
