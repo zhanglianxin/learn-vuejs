54f499dc41727f1734991c89ec0477246d809fc3
zhanglianxin <zhanglianxin1993@qq.com>
2018-02-11 22:47:24 +0800

    Using axios to send a request

---
+++ axios-demo/src/components/dashboard/dashboard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="dashboard">
+    <h1>That's the dashboard!</h1>
+    <p>You should only get here if you're authenticated!</p>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+
+  export default {
+    created() {
+      axios.get('https://vue-update-670a3.firebaseio.com/users.json')
+        .then(res => console.log(res))
+        .catch(error => console.log(error))
+    }
+  }
+</script>
+
+<style scoped>
+  h1, p {
+    text-align: center;
+  }
+
+  p {
+    color: red;
+  }
+</style>
